6e2ccad17fc9fc6dd615074a9e132a7583ffe55e
Vitaly K <vitaly@vitaly.com.ua>
2022-10-21 19:22:24 +0300

    Implement "pipeline" in yaml
    Supports:
    - filter
    - map
    - out
    - one_shot

--- live_py/yaml-parse.py
+++ live_py/yaml-parse.py
@@ -1,6 +1,7 @@
+import logging
 import pprint
-from dataclasses import dataclass
-from typing import Any, List, Optional, Tuple
+import uuid
+from typing import Any, Dict, List, Optional, Tuple, Union
 
 import reactivex
 import yaml
@@ -14,34 +15,44 @@ pp = pprint.PrettyPrinter(indent=4)
 yaml_namespace = {}
 yaml_classes = {}
 
+logger = logging.getLogger(__name__)
+logging.basicConfig(level=logging.DEBUG)
 
-def new_obj(yaml_obj: dict):
+
+def new_obj(yaml_obj: dict) -> Optional[object]:
     assert type(yaml_obj) is dict
     obj_class, obj_name = next(iter(yaml_obj.items()))
-    yaml_namespace[obj_name] = yaml_classes[obj_class](yaml_obj)
-    return yaml_namespace[obj_name]
+
+    if obj_name is None:
+        obj_name = f'{obj_class}-{str(uuid.uuid4())}'
+
+    yaml_class = yaml_classes.get(obj_class, None)
+
+    if yaml_class:
+        yaml_namespace[obj_name] = yaml_classes[obj_class](yaml_obj)
+        return yaml_namespace[obj_name]
+    else:
+        return None
 
 
 class Page:
     def __init__(self, yaml_obj: dict):
-        print(f'Create Page from {yaml_obj}')
+        logger.debug(f'Create Page from {yaml_obj}')
 
-        self.widgets = [new_obj(w) for w in yaml_obj['widgets']]
+        widgets_iter = (new_obj(w) for w in yaml_obj['widgets'])
+        widgets_iter = filter(lambda o: o is not None, widgets_iter)
+
+        self.widgets = list(widgets_iter)
 
 
 class Var:
     def __init__(self, yaml: dict):
-        print(f'Create from {yaml}')
-
-
-class Filter:
-    def __init__(self, yaml: dict):
-        print(f'Create from {yaml}')
+        logger.debug(f'Create from {yaml}')
 
 
 class Button:
     def __init__(self, yaml: dict):
-        print(f'Create from {yaml}')
+        logger.debug(f'Create from {yaml}')
         self.name = next(iter(yaml.items()))[1]
         self.hw = yaml.get('hw', None)
         self.hw_pos = yaml.get('hw_pos', None)
@@ -59,47 +70,157 @@ class Button:
 
 class Clock:
     def __init__(self, yaml: dict):
-        print(f'Create from {yaml}')
+        logger.debug(f'Create from {yaml}')
+
+
+T_VAR_NAME = Union[str, tuple[str, str]]
+
+
+def str_to_var_name(var_name_str: str) -> T_VAR_NAME:
+    if var_name_str == 'input':
+        return var_name_str
+    else:
+        name = tuple(var_name_str.split('.'))
+        return name
+
+
+class Pipeline:
+    def __init__(self, yaml_obj: dict, var_subjects: Dict[str, reactivex.Subject[Any]]) -> None:
+        logger.debug(f'Create Pipeline from: \n{pprint.pformat(yaml_obj)}')
+        self.obs = None
+        self.repr = ""
+        self._repr_parts = []
+        self._var_subjects = var_subjects
+
+        for pipe_element in yaml_obj['pipe']:
+            self._create_pipe_element(pipe_element)
+
+        self.repr = '[ ' + " -> ".join(self._repr_parts) + ' ]'
+
+        assert self.obs
+
+    def _create_pipe_element(self, pipe_element):
+        match pipe_element:
+            case {'map': fn, 'vars': in_vars}:
+                logger.debug(f"Map: {fn}")
+                logger.debug(f"Input vars: {tuple(in_vars.keys())}")
+                self._repr_parts.append(fn)
+
+                in_var_names = self._create_combine(in_vars)
+                assert self.obs
+
+                self.obs = ops.map(self._create_exec_fn(in_var_names, fn))(self.obs)
+
+            case {'filter': fn, 'vars': in_vars}:
+                logger.debug(f"Filter: {fn}")
+                logger.debug(f"Input vars: {tuple(in_vars.keys())}")
+                self._repr_parts.append(fn)
+
+                in_var_names = self._create_combine(in_vars)
+                assert self.obs
+
+                self.obs = ops.filter(self._create_exec_fn(in_var_names, fn))(self.obs)
+
+            case {'out': out_var}:
+                self._repr_parts.append(out_var)
+                out_var = str_to_var_name(out_var)
+
+                logger.debug(f"Out to {out_var}")
+
+                assert self.obs
+
+                self.obs = self.obs.pipe(ops.do_action(
+                    on_next=lambda v: self._var_subjects[out_var].on_next(v)))
+
+            case {'one_shot': value}:
+                logger.debug(f"One shot value: {value}")
+                self._repr_parts.append(str(value))
+
+                assert not self.obs
+
+                self.obs = reactivex.of(value)
+
+    def _parse_input_var_names(self, in_vars: Dict[str, str]) -> List[T_VAR_NAME]:
+        return list(map(str_to_var_name, in_vars.keys()))
+
+    def _create_input_var_names(self, in_vars: Dict[str, str]) -> List[T_VAR_NAME]:
+        in_var_names = self._parse_input_var_names(in_vars)
+
+        for name in in_var_names:
+            if name != 'input':
+                create_var(name)
+
+        return in_var_names
+
+    def _create_combine(self, in_vars: Dict[str, str]) -> List[T_VAR_NAME]:
+        in_var_names = self._create_input_var_names(in_vars)
+
+        def get_obs(var_name):
+            obs = self.obs if var_name == 'input' else self._var_subjects[var_name]
+            assert obs
+            return obs
+
+        self.obs = combine_rx(
+            list(map(get_obs, in_var_names)),
+            [rx_type == 'rx' for rx_type in in_vars.values()],
+            in_var_names
+        )
+
+        return in_var_names
+
+    def _create_exec_fn(self, in_var_names, in_var_fn):
+        input_vars = list(in_var_names)
+        var_fn = str(in_var_fn)
+
+        def exec_fn(args):
+            ns = {}
+
+            for i, input_var in enumerate(input_vars):
+                value = args[i]
+
+                if input_var == 'input':
+                    ns['input'] = value
+                else:
+                    obj_name, attr_name = input_var
+                    obj = ns.get(obj_name, None)
+
+                    if obj is None:
+                        obj = type('', (object,), {})()
+                        ns[obj_name] = obj
+
+                    setattr(obj, attr_name, value)
+
+            result = eval(var_fn, ns)
+
+            logger.debug(f"Run exec. f() = {in_var_fn} = {result}")
+
+            return result
+
+        return exec_fn
 
 
 yaml_classes['page'] = Page
 yaml_classes['button'] = Button
 yaml_classes['var'] = Var
 yaml_classes['clock'] = Clock
-yaml_classes['filter'] = Filter
 
-var_in_subjects = {}
 var_out_subjects = {}
-var_obs = {}
 init_values = {}
+pipelines: List[Pipeline] = []
 
 
 def create_var(var_name):
-    if var_in_subjects.get(var_name, None) is None:
-        print(f'Create in subject for {var_name}')
-        var_in_subjects[var_name] = reactivex.Subject()
-
     if var_out_subjects.get(var_name, None) is None:
-        print(f'Create out subject for {var_name}')
+        logger.debug(f'Create out subject for {var_name}')
         var_out_subjects[var_name] = reactivex.Subject()
-
-    if var_obs.get(var_name, None) is None:
-        print(f'Create ops list for {var_name}')
-        var_obs[var_name] = []
-
-
-@dataclass
-class BindInputVar:
-    name: tuple[str, str]
-    rx: bool
 
 
 def combine_rx(sources: List[Observable[Any]], is_rx: List[bool],
-               from_: List[str]) -> Observable[Tuple[Any, ...]]:
+               from_: List[Union[str, tuple[str, str]]]) -> Observable[Tuple[Any, ...]]:
     parent = sources[0]
     assert len(sources) == len(is_rx)
 
-    print(f'combine_rx: create combine_rx: from {from_}')
+    logger.debug(f'combine_rx: create combine_rx: from {from_}')
 
     def subscribe(
         observer: abc.ObserverBase[Any], scheduler: Optional[abc.SchedulerBase] = None
@@ -107,31 +228,29 @@ def combine_rx(sources: List[Observable[Any]], is_rx: List[bool],
 
         n = len(sources)
         has_value = [False] * n
-        has_value_all = False
+        first_time_all = True
         is_done = [False] * n
         values = [None] * n
 
-        print(f'combine_rx: subscribe to combine_rx: from {from_}')
+        logger.debug(f'combine_rx: subscribe to combine_rx: from {from_}')
 
         def _next(i: Any) -> None:
             has_value[i] = True
-            nonlocal has_value_all
+            nonlocal first_time_all
 
-            print(f'combine_rx: values {values}, has_value {has_value}')
-            print(f'combine_rx: on_next on combine_rx: from {from_}')
+            logger.debug(f'combine_rx: values {values}, has_value {has_value}, from {from_}')
+            logger.debug(f'combine_rx: on_next: from "{from_[i]}" = {values[i]} (is_rx={is_rx[i]})')
 
-            if has_value_all or all(has_value):
-                first_time_all = not has_value_all
-
+            if all(has_value):
                 if first_time_all or is_rx[i]:
+                    logger.debug(
+                        f'combine_rx: pass to observer {first_time_all=}, {is_rx=}, {values=}')
                     res = tuple(values)
-                    print(f'combine_rx: pass to observer')
+                    first_time_all = False
                     observer.on_next(res)
 
             elif all([x for j, x in enumerate(is_done) if j != i]):
                 observer.on_completed()
-
-            has_value_all = all(has_value)
 
         def done(i: Any) -> None:
             is_done[i] = True
@@ -165,83 +284,6 @@ def combine_rx(sources: List[Observable[Any]], is_rx: List[bool],
     return Observable(subscribe)
 
 
-def create_binds(objs):
-    for yaml_obj in objs:
-        bind_list = yaml_obj.get('bind', None)
-
-        if bind_list:
-            print(f'Create binds for {yaml_obj}')
-
-            for bind_obj in bind_list:
-                var_name, var_fn = next(iter(bind_obj.items()))
-                var_name = tuple(var_name.split('.'))
-
-                print(f'Bind: {var_name} <- {var_fn}')
-
-                # combine_subjects = []
-                input_vars: list[BindInputVar] = []
-
-                create_var(var_name)
-
-                for input_var_obj in bind_obj['input']:
-                    input_var, input_var_rx = next(iter(input_var_obj.items()))
-                    input_var = tuple(input_var.split('.'))
-                    input_var_rx = input_var_rx == 'rx'
-                    print(f'  Input var: {input_var} rx = {input_var_rx}')
-
-                    create_var(input_var)
-                    input_vars.append(BindInputVar(input_var, input_var_rx))
-                    # combine_subjects.append(var_out_subjects[input_var])
-
-                def create_exec_fn(in_input_vars, in_var_fn):
-                    input_vars = list(in_input_vars)
-                    var_fn = str(in_var_fn)
-
-                    def exec_fn(args):
-                        ns = {}
-
-                        for i, input_var in enumerate(input_vars):
-                            obj_name, attr_name = input_var.name
-                            obj = ns.get(obj_name, None)
-
-                            if obj is None:
-                                obj = type('', (object,), {})()
-                                ns[obj_name] = obj
-
-                            setattr(obj, attr_name, args[i])
-
-                        return eval(var_fn, ns)
-
-                    return exec_fn
-
-                var_obs[var_name].append(
-                    combine_rx(
-                        [var_out_subjects[v.name] for v in input_vars],
-                        [v.rx for v in input_vars],
-                        [v.name for v in input_vars]
-                    ).pipe(
-                        ops.map(create_exec_fn(input_vars, var_fn))
-                    )
-                )
-
-        if 'widgets' in yaml_obj:
-            create_binds(yaml_obj['widgets'])
-
-
-with open('set.yaml', 'r') as stream:
-    yaml_data = yaml.safe_load(stream)
-
-    for obj in yaml_data:
-        new_obj(obj)
-
-    create_binds(yaml_data)
-
-    # pp.pprint(yaml_data)
-
-
-# print(yaml_namespace)
-
-
 def activity_manager(yaml_namespace):
     active_pages = []
 
@@ -264,10 +306,10 @@ def activity_manager(yaml_namespace):
 
                     if active:
                         active_pages.insert(0, page)
-                        print(f'active_pages (inserted) = {active_pages}')
+                        logger.debug(f'active_pages (inserted) = {active_pages}')
                     elif not active:
                         active_pages.remove(page)
-                        print(f'active_pages (removed) = {active_pages}')
+                        logger.debug(f'active_pages (removed) = {active_pages}')
 
             return source.subscribe(on_next,
                                     observer.on_error,
@@ -279,74 +321,75 @@ def activity_manager(yaml_namespace):
     return _operator
 
 
-var_out_subjects[('clock', 'tempo')].subscribe(
-    on_next=lambda v: print(f'clock.tempo {v}'),
-    on_error=lambda v: print(f'clock.tempo error {v}'),
-    on_completed=lambda: print(f'clock.tempo on_completed')
-)
-
-for var_name in var_out_subjects.keys():
-    if not var_obs[var_name]:
-        var_in_subjects[var_name].subscribe(var_out_subjects[var_name])
-    else:
-        for ob in var_obs[var_name]:
-            print(f'Subscribe {var_name} out subject to its obs')
-            ob.subscribe(var_out_subjects[var_name])
-
-# var_in_subjects[('inc_static', 'value')].on_next(2)
-# var_in_subjects[('shift_inc', 'value')].on_next(1)
-
-# var_in_subjects[('shift_inc', 'value')].on_next(3)
-# var_in_subjects[('inc_static', 'value')].on_next(4)
-# var_in_subjects[('shift_inc', 'value')].on_next(5)
-# var_in_subjects[('inc_static', 'value')].on_next(4)
-# var_in_subjects[('shift_inc', 'value')].on_next(3)
-
-# var_in_subjects[('shift_inc', 'value')].on_next(9)
-# var_in_subjects[('shift_inc', 'value')].on_next(9)
-# var_in_subjects[('shift_inc', 'value')].on_next(9)
-# var_in_subjects[('inc_static', 'value')].on_next(0)
-# var_in_subjects[('inc_static', 'value')].on_next(0)
-# var_in_subjects[('shift_inc', 'value')].on_next(10)
-# var_in_subjects[('inc_static', 'value')].on_next(1000)
-# var_in_subjects[('inc_static', 'value')].on_next(1001)
-
 def route_to_var(msg):
-    var_subject = var_in_subjects.get(msg[0], None)
+    var_subject = var_out_subjects.get(msg[0], None)
 
     if var_subject is not None:
-        print(f'route {msg[1]} to {msg[0]}')
+        logger.debug(f'route {msg[1]} to {msg[0]}')
         var_subject.on_next(msg[1])
+
+
+with open('set-pipelines.yaml', 'r') as stream:
+    yaml_data = yaml.safe_load(stream)
+
+    for yaml_obj in yaml_data:
+        new_obj(yaml_obj)
+
+    for yaml_obj in yaml_data:
+        if 'pipeline' in yaml_obj:
+            pipelines.append(Pipeline(yaml_obj, var_out_subjects))
+
+
+var_out_subjects[('clock', 'tempo')].subscribe(
+    on_next=lambda v: logger.info(f'clock.tempo = {v}')
+)
+
+for pipeline in pipelines:
+    pipeline.obs.subscribe(
+        on_next=lambda v, pipe=pipeline.repr: logger.debug(f'pipeline {pipe} on_next: {v}'),
+        on_completed=lambda: logger.debug(f'pipeline on_completed'),
+        on_error=lambda e: logger.debug(f'pipeline error {e}'))
+
 
 reactivex.of(
     ('active', 'main', True),
-    ('midicc', 'LaunchPad1', 1, 127),
+    ('midicc', 'LaunchPad1', 1, 127),  # tempo dec
     ('midicc', 'LaunchPad1', 1, 0),
-    ('midicc', 'LaunchPad1', 2, 127),
+    ('midicc', 'LaunchPad1', 2, 127),  # tempo inc
     ('midicc', 'LaunchPad1', 2, 0),
-    ('midicc', 'LaunchPad1', 3, 127),
-    ('midicc', 'LaunchPad1', 3, 0),
-    ('midicc', 'LaunchPad1', 1, 127),
+    ('midicc', 'LaunchPad1', 3, 127),  # shift x10 pressed
+    ('midicc', 'LaunchPad1', 1, 127),  # tempo dec
     ('midicc', 'LaunchPad1', 1, 0),
-
-
-    ('active', 'shuffle', True),
-
-    ('midicc', 'LaunchPad1', 2, 127),
+    ('midicc', 'LaunchPad1', 1, 127),  # tempo dec
+    ('midicc', 'LaunchPad1', 1, 0),
+    ('midicc', 'LaunchPad1', 2, 127),  # tempo inc
     ('midicc', 'LaunchPad1', 2, 0),
-
-    ('active', 'shuffle', False),
-
-    ('midicc', 'LaunchPad1', 2, 127),
+    ('midicc', 'LaunchPad1', 3, 0),  # shift x10 released
+    ('midicc', 'LaunchPad1', 2, 127),  # tempo inc
     ('midicc', 'LaunchPad1', 2, 0),
+    # ('active', 'shuffle', True),
 
-    ('midicc', 'LaunchPad2', 2, 0),
-    ('midicc', 'LaunchPad2', 2, 0),
-    ('midicc', 'LaunchPad2', 1, 0),
-).pipe(activity_manager(yaml_namespace),
-       ops.do_action(on_next=route_to_var)).subscribe(
-    on_next=lambda v: print(f'Activity Manager on_next: {v}'),
-    on_completed=lambda: print(f'Activity Manager on_completed')
-)
+    # ('midicc', 'LaunchPad1', 2, 127),
+    # ('midicc', 'LaunchPad1', 2, 0),
 
-# reactivex.case
+    # ('active', 'shuffle', False),
+
+    # ('midicc', 'LaunchPad1', 2, 127),
+    # ('midicc', 'LaunchPad1', 2, 0),
+
+    # ('midicc', 'LaunchPad2', 2, 0),
+    # ('midicc', 'LaunchPad2', 2, 0),
+    # ('midicc', 'LaunchPad2', 1, 0),
+
+).pipe(
+    activity_manager(
+        yaml_namespace
+    ),
+    ops.do_action(
+        on_next=lambda v: logger.debug(f'Activity Manager out: {v}'),
+        on_completed=lambda: logger.debug(f'Activity Manager on_completed')
+    ),
+    ops.do_action(
+        on_next=route_to_var
+    ),
+).subscribe()
